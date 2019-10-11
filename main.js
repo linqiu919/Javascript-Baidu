bf8235f69323124baef0c7b261e2b5d22535b0d4
mcmyth <1440126177@qq.com>
2019-10-11 20:23:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,147 @@
+var head = document.head || document.getElementsByTagName('head')[0];
+let device = /Android|webOS|iPhone|iPod|BlackBerry|iPad/i;
+if(device.test(navigator.userAgent)) {
+    var script = document.createElement('script');
+    script.setAttribute("src", "mobile/script.js");
+    head.appendChild(script);
+    var link = document.createElement('link');
+    link.href = "mobile/style.css";
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    head.appendChild(link);
+
+
+} else {
+    var script = document.createElement('script');
+    script.setAttribute("src", "pc/script.js");
+    head.appendChild(script);
+    var link = document.createElement('link');
+    link.href = "pc/style.css";
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    head.appendChild(link);
+}
+window.onload = function () {
+
+    if(device.test(navigator.userAgent)) {
+
+        document.getElementById("logo").src="images/plus_logo_web.png";
+        document.getElementById("search_input").value="";
+    }else
+    {
+        document.getElementById("logo").src="images/bd_logo1.png";
+        document.getElementById("search_input").focus();
+        document.getElementById("search_input").value="";
+        document.getElementById("ret").style.display="none";
+        showSearchBoard(true);
+
+    }
+
+
+
+
+
+};
+
+function checkKey(evt) {
+    var keyID = (evt.charCode) ? evt.charCode : ((evt.which) ? evt.which : evt.keyCode);
+    return  keyID;
+}
+
+function setCookie(name,value,time)
+{
+    var strsec = getsec(time);
+    var exp = new Date();
+    exp.setTime(exp.getTime() + strsec*1);
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+
+}
+function getsec(str)
+{
+    var str1=str.substring(1,str.length)*1;
+    var str2=str.substring(0,1);
+    if (str2=="s")
+    {
+        return str1*1000;
+    }
+    else if (str2=="h")
+    {
+        return str1*60*60*1000;
+    }
+    else if (str2=="d")
+    {
+        return str1*24*60*60*1000;
+    }
+}
+
+//读取cookies
+function getCookie(name)
+{
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+
+    if(arr=document.cookie.match(reg))
+
+        return unescape(arr[2]);
+    else
+        return null;
+}
+
+//删除cookies
+function delCookie(name)
+{
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval=getCookie(name);
+    if(cval!=null)
+        document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+}
+
+
+function searchHistory(mode,value){
+    let history = new Array();
+    let history_json = getCookie("history");
+
+if(mode==="add")
+{
+    if(history_json !== null){
+    history = JSON.parse(history_json);
+    }
+    history.remove(value);
+    history.unshift(value);
+
+    if(history.length>10)
+    {
+        history.splice(11, history.length - 11);
+    }
+history_json = JSON.stringify(history);
+//console.log(history_json);
+    setCookie("history",history_json,"d30");
+}
+    if(mode==="get")
+    {
+       history = JSON.parse(history_json);
+//       console.log(history);
+       return history;
+    }
+
+    if(mode==="del")
+    {
+        history = JSON.parse(history_json);
+        history.remove(value);
+        history_json = JSON.stringify(history);
+        setCookie("history",history_json,"d30");
+        //console.log(history_json);
+    }
+
+    if(mode==="del_all")
+    {
+        setCookie("history","","s0");
+    }
+}
+
+Array.prototype.remove = function(val) {
+    var index = this.indexOf(val);
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+};
---
+++ pc/script.js
@@ -0,0 +1,189 @@
+let isDel=false;
+function  showSearchBoard(show)
+{
+
+    let wd = document.getElementById("search_input").value;
+    if(show)
+    {
+    document.getElementById("search_box").classList.add("search_box_focus");
+        if(wd !== "")
+        {
+            document.getElementById("ret").style.display="block";
+            document.getElementById("history").style.display="none";
+        }else
+        {
+            let history = searchHistory("get");
+            let listHTML = "";
+           // console.log(history);
+
+            if(history !== null &&  history.length > 0 ) {
+                document.getElementById("history").style.display = "block";
+
+            for(let i=0;i< history.length;i++){
+                listHTML+='<li class="history">'+history[i]+'<span class="del">删除</span></li>';
+                document.getElementById("history_caontainer").innerHTML=listHTML;
+            }
+
+                let list = document.getElementsByClassName("history");
+
+                for (var i = 0; i < list.length; i++) {
+
+                    list[i].addEventListener("click", function (e) {
+                        e.preventDefault();
+                        if(isDel===false){
+                        let parent_str = this.innerText.substr(0, this.innerText.length - 3);
+                       // console.log(parent_str);
+                        //searchHistory("add",parent_str);
+                       window.location = "https://www.baidu.com/s?wd="+parent_str;
+                        }
+
+                    });
+                }
+
+
+          list = document.getElementsByClassName("del");
+            for (var i = 0; i < list.length; i++) {
+                list[i].addEventListener("click", function (e) {
+                    e.preventDefault();
+                    let parent = this.parentNode;
+                    let parent_str = parent.innerText.substr(0, parent.innerText.length - 3);
+                   // console.log(parent_str);
+                    searchHistory("del",parent_str);
+                    document.getElementById("history_caontainer").removeChild(parent);
+                    isDel = true;
+                });
+            }
+            }
+        }
+
+    }else
+    {
+        document.getElementById("search_box").classList.remove("search_box_focus");
+
+    }
+}
+
+function getRet()
+{
+    let wd = document.getElementById("search_input").value;
+    if(wd !== ""){
+        document.getElementById("history").style.display="none";
+        let script = document.createElement("script");
+        script.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd="+wd+"&cb=retResponse";
+        document.body.insertBefore(script, document.body.firstChild);
+        document.body.removeChild(script);
+
+
+    }else {
+        let history = searchHistory("get");
+
+        if(history !== null &&  history.length > 0 ) {
+            document.getElementById("history").style.display = "block";
+
+        }
+        document.getElementById("ret").style.display = "none";
+    }
+
+
+}
+document.body.addEventListener("click", function (e) {
+    if(event.target.id !== "search_input"){
+    document.getElementById("ret").style.display="none";
+        document.getElementById("history").style.display="none";
+    }
+
+
+
+}, false);
+function retResponse(ret){
+    let listHTML="";
+    let wd = document.getElementById("search_input").value;
+    document.getElementById("ret_caontainer").innerHTML="";
+    for(let i=0;i< ret.s.length;i++){
+        //console.log(ret.s[i]);
+        listHTML+="<li class='ret'>"+getStr(ret.s[i],wd)+"</li>";
+        document.getElementById("ret_caontainer").innerHTML=listHTML;
+    }
+
+    let list = document.getElementsByClassName("ret");
+
+    for (var i = 0; i < list.length; i++) {
+
+        list[i].addEventListener("click", function (e) {
+            e.preventDefault();
+            searchHistory("add",this.innerText);
+            window.location = "https://www.baidu.com/s?wd="+this.innerText;
+        });
+    }
+    if(list.length === 0)
+    {
+        document.getElementById("ret").style.display="none";
+    }else
+    {
+        document.getElementById("ret").style.display="block";
+    }
+}
+
+
+function getStr(string,str){
+    var str_before = string.split(str)[0];
+    var str_after = string.split(str)[1];
+    if(string.search(str) !== -1 )
+        return '<b>'+str_before+'</b>'+str+'<b>'+str_after+'</b>';
+    else
+        return  string;
+
+}
+function searchWd()
+{
+    let wd = document.getElementById("search_input").value;
+    if(wd !== "") {
+        searchHistory("add",wd);
+     window.location = "https://www.baidu.com/s?wd=" + wd;
+    }
+}
+
+let item_id=0;
+document.getElementById("search_input").addEventListener("keydown",function (event) {
+
+    if( document.getElementById("ret_caontainer").innerHTML!== ""){
+        for(let i=0; i < document.getElementsByClassName("ret").length;i++)
+        {
+            document.getElementsByClassName("ret")[i].classList.remove("ret_hover");
+        }
+        if(checkKey(event)===40){
+
+            if(item_id < document.getElementsByClassName("ret").length - 1)
+            {
+                item_id++;
+            }else
+            {
+                item_id=0;
+            }
+document.getElementsByClassName("ret")[item_id].classList.add("ret_hover");
+            document.getElementById("search_input").value=document.getElementsByClassName("ret")[item_id].innerText;
+    }
+
+        if(checkKey(event)===38){
+            if(item_id > 0)
+            {
+                item_id--;
+            }else
+            {
+                item_id=document.getElementsByClassName("ret").length - 1;
+            }
+            document.getElementsByClassName("ret")[item_id].classList.add("ret_hover");
+            document.getElementById("search_input").value=document.getElementsByClassName("ret")[item_id].innerText;
+        }
+}
+
+
+});
+
+
+document.getElementById("ret_caontainer").addEventListener("mouseover",function (event) {
+    for(let i=0; i < document.getElementsByClassName("ret").length;i++)
+    {
+        document.getElementsByClassName("ret")[i].classList.remove("ret_hover");
+    }
+});
